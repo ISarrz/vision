4c9091ae01fbae0f1245acba5c85ed8d241da106
German <germanzlobin2@gmail.com>
2023-10-18 20:53:08 +0700

    image

--- dataset.py
+++ dataset.py
@@ -14,12 +14,14 @@ def extract_letters(img):
     filterd_image = cv2.GaussianBlur(im, (5, 5), 0)
     filterd_image = cv2.fastNlMeansDenoising(filterd_image, None, 20, 7, 21)
     img_grey = cv2.cvtColor(filterd_image, cv2.COLOR_BGR2GRAY)
-
-    thresh = 140
+    cv2.imshow("", img_grey)
+    cv2.waitKey(0)
+    thresh = 100
 
     # get threshold image
     ret, thresh_img = cv2.threshold(img_grey, thresh, 255, cv2.THRESH_BINARY)
-
+    cv2.imshow("", thresh_img)
+    cv2.waitKey(0)
     # output = thresh_img.copy()
     # find contours
     contours, hierarchy = cv2.findContours(thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
@@ -37,17 +39,19 @@ def extract_letters(img):
         diag = sqrt(w ** 2 + h ** 2)
         if hierarchy[0][ind][3] == 0 and diag >= sr_diag:
             croped = thresh_img[y:y + h, x:x + w]
-            size_max = int(max(w, h) * 1.1)
+            size_max = int(max(w, h) * 1.4)
             letter_square = 255 * np.ones(shape=[size_max, size_max], dtype=np.uint8)
 
             # центрируем
             x_0 = (size_max - w) // 2
             y_0 = (size_max - h) // 2
             letter_square[y_0:y_0 + h, x_0:x_0 + w] = croped
-            letter_square = cv2.resize(letter_square, (28 * 10, 28 * 10), interpolation=cv2.INTER_AREA)
-            letter_square = ~letter_square
+            letter_square = cv2.resize(letter_square, (28, 28), interpolation=cv2.INTER_AREA)
+
             letter_square = cv2.flip(letter_square, 1)
             letter_square = cv2.rotate(letter_square, cv2.ROTATE_90_COUNTERCLOCKWISE)
+            letter_square = 1 - letter_square / 255
+            cv2.imshow("", letter_square)
             letters.append(letter_square)
             # cv2.rectangle(output, (x, y), (x + w, y + h), (70, 0, 0), 1)
 
--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import json
-
+import keras
 from dataset import extract_letters
 import cv2
-
-
+import numpy as np
 
 if __name__ == "__main__":
     with open("encodes.json", 'r') as file:
         encodes = json.load(file)
-    im = cv2.imread("text.jpg")
-    letters = extract_letters(im)
+    model = keras.models.load_model('emnist_letters.h5')
+    letters = extract_letters("text.jpg")
+    for i in letters:
+        img_arr = np.expand_dims(i, axis=0)
+        #img_arr = img_arr / 255.0
+
+        #img_arr = img_arr.reshape((1, 28, 28, 1))
+        predict = model.predict([img_arr])
+        result = np.argmax(predict, axis=1)
+        print(chr(encodes[str(result[0])]))
+        cv2.imshow("", i)
+        cv2.waitKey(0)
