d91d250db18d0f0414bea6cbbae9ba530464a98a
German <germanzlobin2@gmail.com>
2023-10-20 14:32:08 +0700

    contour problem

--- dataset.py
+++ dataset.py
@@ -8,7 +8,7 @@ from math import sqrt
 
 def extract_letters(img):
     letters = []
-    im = cv2.imread(img)
+    im = img
 
     ##  filterd_image = cv2.medianBlur(my_photo, 3)
     filterd_image = cv2.GaussianBlur(im, (5, 5), 0)
@@ -16,16 +16,18 @@ def extract_letters(img):
     img_grey = cv2.cvtColor(filterd_image, cv2.COLOR_BGR2GRAY)
     cv2.imshow("", img_grey)
     cv2.waitKey(0)
-    thresh = 100
+    thresh = 180
 
     # get threshold image
     ret, thresh_img = cv2.threshold(img_grey, thresh, 255, cv2.THRESH_BINARY)
     cv2.imshow("", thresh_img)
+    #output = img.copy()
     cv2.waitKey(0)
     # output = thresh_img.copy()
     # find contours
     contours, hierarchy = cv2.findContours(thresh_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     sr_diag = 0
+    if len(contours) <= 1: return []
 
     for ind, contour in enumerate(contours[1:]):
         if hierarchy[0][ind][3] == 0:
@@ -37,7 +39,8 @@ def extract_letters(img):
     for ind, contour in enumerate(contours):
         (x, y, w, h) = cv2.boundingRect(contour)
         diag = sqrt(w ** 2 + h ** 2)
-        if hierarchy[0][ind][3] == 0 and diag >= sr_diag:
+        #if hierarchy[0][ind][3] == 0 and diag >= sr_diag:
+        if hierarchy[0][ind][3] == 0:
             croped = thresh_img[y:y + h, x:x + w]
             size_max = int(max(w, h) * 1.4)
             letter_square = 255 * np.ones(shape=[size_max, size_max], dtype=np.uint8)
@@ -52,8 +55,9 @@ def extract_letters(img):
             letter_square = cv2.rotate(letter_square, cv2.ROTATE_90_COUNTERCLOCKWISE)
             letter_square = 1 - letter_square / 255
             cv2.imshow("", letter_square)
-            letters.append(letter_square)
-            # cv2.rectangle(output, (x, y), (x + w, y + h), (70, 0, 0), 1)
+            cv2.waitKey(0)
+            letters.append([(x,y,w,h),letter_square])
+            #cv2.rectangle(output, (x, y), (x + w, y + h), (70, 0, 0), 1)
 
     return letters
 
--- main.py
+++ main.py
@@ -3,19 +3,61 @@ import keras
 from dataset import extract_letters
 import cv2
 import numpy as np
+from flask import Flask, render_template, Response
+import cv2
+import time
+
+with open("encodes.json", 'r') as file:
+    encodes = json.load(file)
+model = keras.models.load_model('emnist_letters.h5')
+app = Flask(__name__)
+camera = cv2.VideoCapture("2.mp4")
+green = (50, 255, 0)
+
+
+
+
+def generate_frames():
+    start_time = time.time()
+    while True:
+
+        ## read the camera frame
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+
+            if time.time() - start_time > 0.2:
+                start_time = time.time()
+                letters = extract_letters(frame)
+
+                for i in letters:
+                    img_arr = np.expand_dims(i[1], axis=0)
+                    # img_arr = img_arr / 255.0
+
+                    # img_arr = img_arr.reshape((1, 28, 28, 1))
+                    predict = model.predict([img_arr])
+                    result = np.argmax(predict, axis=1)
+                    text = chr(encodes[str(result[0])])
+                    cv2.rectangle(frame, (i[0][0], i[0][1]), (i[0][0] + i[0][2], i[0][1] + i[0][3]), green, 3)
+                    cv2.putText(frame, text, (i[0][0], i[0][1]), cv2.FONT_HERSHEY_SIMPLEX, 2, green, 3)
+
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/video')
+def video():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == "__main__":
-    with open("encodes.json", 'r') as file:
-        encodes = json.load(file)
-    model = keras.models.load_model('emnist_letters.h5')
-    letters = extract_letters("text.jpg")
-    for i in letters:
-        img_arr = np.expand_dims(i, axis=0)
-        #img_arr = img_arr / 255.0
-
-        #img_arr = img_arr.reshape((1, 28, 28, 1))
-        predict = model.predict([img_arr])
-        result = np.argmax(predict, axis=1)
-        print(chr(encodes[str(result[0])]))
-        cv2.imshow("", i)
-        cv2.waitKey(0)
+    app.run(debug=True)
